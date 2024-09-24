da051ff9c252c603bb5a894150e6aa45a98446ad
GStudies <aquinogerardoXIII@gmail.com>
2024-09-25 00:44:47 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import { Command } from 'commander';
-import { listContacts, getContactById, removeContact, addContact } from "./contacts.js";
-
+import { Command } from "commander";
 const program = new Command();
 
+
+import { getContactById, listContacts, removeContact, addContact } from "./contacts.js";
+
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone")
-  .allowUnknownOption(true);
+.option("-a, --action <type>", "choose action")
+.option("-i, --id <type>", "user id")
+.option("-n, --name <type>", "user name")
+.option("-e, --email <type>", "user email")
+.option("-p, --phone <type>", "user phone")
+.allowUnknownOption(true);
 
 program.parse();
 
@@ -18,28 +19,28 @@ const argv = program.opts();
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
-          const contacts = await listContacts();
-          console.table(contacts);
+      const contacts = await listContacts();
+      console.table(contacts);
       break;
 
     case "get":
-          const contactsById = await getContactById(id);
-          console.table(contactsById);
+      const contactsById = await getContactById(id);
+      console.log(contactsById);
       break;
 
     case "add":
-          const addNewContact = await addContact(name, email, phone);
-          console.log(addNewContact);
+      const addNewContact = await addContact({ name, email, phone });
+      console.log(addNewContact);
       break;
 
     case "remove":
-          const removedContact = await removeContact(id);
-          console.log(removedContact);
+      const deletedContact = await removeContact(id);
+      console.log(deletedContact);
       break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
-}
+};
 
 invokeAction(argv);
